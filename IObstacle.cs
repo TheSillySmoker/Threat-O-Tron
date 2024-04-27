6fd0b036c79ad457a0014b526feeca58a2ff68cc
TheSillySmoker <69103396+TheSillySmoker@users.noreply.github.com>
2024-04-27 20:41:44 +1000

    delete it all
    Issues committing. Going to delete all files and try again.

--- IObstacle.cs
+++
@@ -1,9 +0,0 @@
-namespace Threat_o_tron;
-
-interface IObstacle{
-    /// <summary>
-    /// Takes an existing map's canvas and plots obstacles onto it.
-    /// </summary>
-    /// <param name="map">The map that will be drawn on.</param>
-    public void drawOnMap(Map map);
-}
