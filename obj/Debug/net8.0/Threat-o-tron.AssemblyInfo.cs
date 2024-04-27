600a1aac7684735bfd22531a8b134c220c85386d
TheSillySmoker <69103396+TheSillySmoker@users.noreply.github.com>
2024-04-27 21:02:01 +1000

    c

--- obj/Debug/net8.0/Threat-o-tron.AssemblyInfo.cs
+++ obj/Debug/net8.0/Threat-o-tron.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Threat-o-tron")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327e7c8d744843da4290b7e8df974c736a2129d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e453fb0b8eeca9229d1f0d5aadcdd8a2c45f7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Threat-o-tron")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Threat-o-tron")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
